69d0d285510d7eee6e1989e7fd2a25787acc45e4
DanialAhmad <dani123.da1@gmail.com>
2021-07-10 05:29:35 +0500

    new commmit

---
+++ ui/whiteboard-collab/src/components/container/Container.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import Board from '../board/Board';
+
+import './style.css';
+
+class Container extends React.Component
+{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            color: "#000000",
+            size: "5"
+        }
+    }
+
+    changeColor(params) {
+        this.setState({
+            color: params.target.value
+        })
+    }
+
+    changeSize(params) {
+        this.setState({
+            size: params.target.value
+        })
+    }
+
+    render() {
+
+        return (
+            <div className="container">
+                <div class="tools-section">
+                    <div className="color-picker-container">
+                        Select Brush Color : &nbsp; 
+                        <input type="color" value={this.state.color} onChange={this.changeColor.bind(this)}/>
+                    </div>
+                    <div className = "eraser">
+                        Eraser 
+                        <input type = "color" value = "#ffffff" onClick = {(e)=> {
+                            e.preventDefault()
+                            this.setState({
+                                color:e.target.value
+                            })
+                        }}/>
+
+                    </div>
+
+                    <div className="brushsize-container">
+                        Select Brush Size : &nbsp; 
+                        <select value={this.state.size} onChange={this.changeSize.bind(this)}>
+                            <option> 5 </option>
+                            <option> 10 </option>
+                            <option> 15 </option>
+                            <option> 20 </option>
+                            <option> 25 </option>
+                            <option> 30 </option>
+                        </select>
+                    </div>
+
+                </div>
+
+                <div class="board-container">
+                    <Board color={this.state.color} size={this.state.size}></Board>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Container
